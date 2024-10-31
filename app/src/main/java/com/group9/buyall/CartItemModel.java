239dd15d003f6a1a863fc707741b9da98f0735ea
duykhanh231 <duykhanh.doc@gmail.com>
2024-10-31 20:16:09 +0700

    Add Cart UI

---
+++ app/src/main/java/com/group9/buyall/CartItemModel.java
@@ -0,0 +1,95 @@
+package com.group9.buyall;
+
+public class CartItemModel {
+
+    public static final int CART_ITEM = 0;
+    public static final int TOTAL_AMOUNT = 1;
+
+    private int type;
+
+    ///cart item
+    private int productImage;
+    private String productTitle;
+    private int freeCoupons;
+    private String productPrice;
+    private String cuttedPrice;
+    private int productQuantity;
+    private int offersApplied;
+    private int couponsApplied;
+
+    public CartItemModel(int type, int productImage, String productTitle, int freeCoupons, String productPrice, String cuttedPrice, int productQuantity, int offersApplied, int couponsApplied) {
+        this.type = type;
+        this.productImage = productImage;
+        this.productTitle = productTitle;
+        this.freeCoupons = freeCoupons;
+        this.productPrice = productPrice;
+        this.cuttedPrice = cuttedPrice;
+        this.productQuantity = productQuantity;
+        this.offersApplied = offersApplied;
+        this.couponsApplied = couponsApplied;
+    }
+
+    public int getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(int productImage) {
+        this.productImage = productImage;
+    }
+
+    public String getProductTitle() {
+        return productTitle;
+    }
+
+    public void setProductTitle(String productTitle) {
+        this.productTitle = productTitle;
+    }
+
+    public int getFreeCoupons() {
+        return freeCoupons;
+    }
+
+    public void setFreeCoupons(int freeCoupons) {
+        this.freeCoupons = freeCoupons;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getCuttedPrice() {
+        return cuttedPrice;
+    }
+
+    public void setCuttedPrice(String cuttedPrice) {
+        this.cuttedPrice = cuttedPrice;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(int productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+    public int getCouponsApplied() {
+        return couponsApplied;
+    }
+
+    public void setCouponsApplied(int couponsApplied) {
+        this.couponsApplied = couponsApplied;
+    }
+
+    public int getOffersApplied() {
+        return offersApplied;
+    }
+
+    public void setOffersApplied(int offersApplied) {
+        this.offersApplied = offersApplied;
+    }
+}
